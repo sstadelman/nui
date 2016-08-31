ba77283c9b8eafc416a3d9c188e624f1331419f0
Stadelman, Stan <stan.stadelman@sap.com>
2016-08-31 16:58:55 -0700

    set public headers

--- NUI/NUI.h
+++ NUI/NUI.h
@@ -16,6 +16,14 @@ FOUNDATION_EXPORT const unsigned char NUIVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like
 
+#import <NUI/NUIAppearance.h>
+#import <NUI/NUIConstants.h>
+#import <NUI/NUIConverter.h>
+#import <NUI/NUIFileMonitor.h>
+#import <NUI/NUIGraphics.h>
+#import <NUI/NUIRenderer.h>
 #import <NUI/NUISettings.h>
-
+#import <NUI/NUIStyleParser.h>
+#import <NUI/NUISwizzler.h>
+#import <NUI/NUIUtilities.h>
 
