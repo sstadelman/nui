d9a3745f0a045f2d538c98cd8b0c8a92f5f40ced
Stadelman, Stan <stan.stadelman@sap.com>
2016-08-31 17:13:56 -0700

    made renderers and extensions public

--- NUI/NUI.h
+++ NUI/NUI.h
@@ -16,6 +16,7 @@ FOUNDATION_EXPORT const unsigned char NUIVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like
 
+// Mark:  Core
 #import <NUI/NUIAppearance.h>
 #import <NUI/NUIConstants.h>
 #import <NUI/NUIConverter.h>
@@ -27,3 +28,49 @@ FOUNDATION_EXPORT const unsigned char NUIVersionString[];
 #import <NUI/NUISwizzler.h>
 #import <NUI/NUIUtilities.h>
 
+// Mark: Renderers
+#import <NUI/NUIActivityIndicatorRenderer.h>
+#import <NUI/NUIBarButtonItemRenderer.h>
+#import <NUI/NUIButtonRenderer.h>
+#import <NUI/NUIControlRenderer.h>
+#import <NUI/NUILabelRenderer.h>
+#import <NUI/NUINavigationBarRenderer.h>
+#import <NUI/NUINavigationItemRenderer.h>
+#import <NUI/NUIPageControlRenderer.h>
+#import <NUI/NUIProgressViewRenderer.h>
+#import <NUI/NUISearchBarRenderer.h>
+#import <NUI/NUISegmentedControlRenderer.h>
+#import <NUI/NUISliderRenderer.h>
+#import <NUI/NUISwitchRenderer.h>
+#import <NUI/NUITabBarItemRenderer.h>
+#import <NUI/NUITabBarRenderer.h>
+#import <NUI/NUITableViewCellRenderer.h>
+#import <NUI/NUITableViewRenderer.h>
+#import <NUI/NUITextFieldRenderer.h>
+#import <NUI/NUITextInputTraitsRenderer.h>
+#import <NUI/NUIToolbarRenderer.h>
+#import <NUI/NUIViewRenderer.h>
+#import <NUI/NUIWindowRenderer.h>
+
+// Mark: UI
+#import <NUI/UIActivityIndicatorView+NUI.h>
+#import <NUI/UIBarButtonItem+NUI.h>
+#import <NUI/UIButton+NUI.h>
+#import <NUI/UIControl+NUI.h>
+#import <NUI/UILabel+NUI.h>
+#import <NUI/UINavigationBar+NUI.h>
+#import <NUI/UINavigationItem+NUI.h>
+#import <NUI/UIPageControl+NUI.h>
+#import <NUI/UIProgressView+NUI.h>
+#import <NUI/UISearchBar+NUI.h>
+#import <NUI/UISegmentedControl+NUI.h>
+#import <NUI/UISlider+NUI.h>
+#import <NUI/UITabBar+NUI.h>
+#import <NUI/UITabBarItem+NUI.h>
+#import <NUI/UITableView+NUI.h>
+#import <NUI/UITableViewCell+NUI.h>
+#import <NUI/UITextField+NUI.h>
+#import <NUI/UITextView+NUI.h>
+#import <NUI/UIToolbar+NUI.h>
+#import <NUI/UIView+NUI.h>
+#import <NUI/UIWindow+NUI.h>
